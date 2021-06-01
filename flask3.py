ee75a312e5f16bc177309100ede218fd37771fcd
fishPointer <fishPointer@protonmail.com>
2021-06-01 16:39:13 -0700

    Used Jinja to create conditional logic & pass variables to template

--- flask3.py
+++ flask3.py
@@ -4,7 +4,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('blog.html', author = "Bob") #you can pass a variable declaration into the parameters of the function and it will reference that in the html file's {{var}} calls
+    return render_template('blog.html', author = "Bob", sunny=False) #you can pass a variable declaration into the parameters of the function and it will reference that in the html file's {{var}} calls
     # make sure you return the output of render_template, and then simply call the html file in /templates
+
+@app.route('/blog')
+def blog():
+    posts = [{'title': 'Technology Time', 'author': 'Ian'}, {'title': 'Brainspasms', 'author':'Eein'}]
+    return render_template('blog.html', author = "Jim", sunny=True, posts=posts)
+
+
 if __name__ == '__main__':
     app.run()

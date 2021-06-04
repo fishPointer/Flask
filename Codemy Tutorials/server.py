f3849934c623bb4e585c7ebadad698c7c765639a
fishPointer <fishPointer@protonmail.com>
2021-06-03 18:11:59 -0700

    Added error rebound to the form, and defaults for anon and blank subject

--- Codemy Tutorials/server.py
+++ Codemy Tutorials/server.py
@@ -22,8 +22,17 @@ def form():
 @app.route('/submit', methods=["POST"])
 def submission():
     username = request.form.get("username")
+    if not username:
+        username = "Anonymous"
     subject = request.form.get("subject")
+    if not subject:
+        subject = "None"
     message = request.form.get("message")
+
+    if not message:
+        error_statement = "Please fill out the Message field..."
+        return render_template('form.html', username=username, subject=subject, message=message, posts=posts, error_statement=error_statement)
+
     posts.append(f"Name: {username} - Subject: {subject} | {message}")
     return render_template('submission.html', username=username, subject=subject, message=message, posts=posts)
 
